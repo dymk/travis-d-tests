23ca001052a3487552dddbdb941fa2a36a43a484
Dylan Knutson <tcdknutson@gmail.com>
2013-12-15 14:38:03 -0800

    tar not gunzip

--- travis_scripts/try_install_ldmd2.sh
+++ travis_scripts/try_install_ldmd2.sh
@@ -3,7 +3,7 @@
 if [ "$DC" == "ldmd2" ]
 then
 	wget https://github.com/ldc-developers/ldc/releases/download/v0.12.1/ldc2-0.12.1-linux-x86_64.tar.gz
-	gunzip ldc2-0.12.1-linux-x86_64.tar.gz
+	tar zxvf ldc2-0.12.1-linux-x86_64.tar.gz
 	echo "adding to path: $(pwd)/ldc2-0.12.1-linux-x86_64/bin"
 	export PATH=$PATH:$(pwd)/ldc2-0.12.1-linux-x86_64/bin
 	ls $(pwd)/ldc2-0.12.1-linux-x86_64/bin
