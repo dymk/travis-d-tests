592cef75f4342f139ae7ac86cf08fa208b67e6f3
Dylan Knutson <tcdknutson@gmail.com>
2013-12-15 14:48:42 -0800

    remove comments

--- travis_scripts/try_install_ldmd2.sh
+++ travis_scripts/try_install_ldmd2.sh
@@ -4,8 +4,5 @@ if [ "$DC" == "ldmd2" ]
 then
 	wget https://github.com/ldc-developers/ldc/releases/download/v0.12.1/ldc2-0.12.1-linux-x86_64.tar.gz
 	tar zxvf ldc2-0.12.1-linux-x86_64.tar.gz
-	echo "adding to path: $(pwd)/ldc2-0.12.1-linux-x86_64/bin"
 	export PATH=$PATH:$(pwd)/ldc2-0.12.1-linux-x86_64/bin
-	ls $(pwd)/ldc2-0.12.1-linux-x86_64/bin
-	echo "ldmd exists at: $(which ldmd2)"
 fi
