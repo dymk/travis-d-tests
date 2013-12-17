07c79b25b3cc4d2b509ca63dad2490404839299a
Dylan Knutson <tcdknutson@gmail.com>
2013-12-16 18:44:35 -0800

    try building master llvm

--- .travis_scripts/install_ldmd2.sh
+++ .travis_scripts/install_ldmd2.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-wget https://github.com/ldc-developers/ldc/releases/download/v0.12.1/ldc2-0.12.1-linux-x86_64.tar.gz
-tar zxvf ldc2-0.12.1-linux-x86_64.tar.gz
-export PATH=$PATH:$(pwd)/ldc2-0.12.1-linux-x86_64/bin
+sudo apt-get install cmake
+git clone https://github.com/ldc-developers/ldc.git ldc
+cd ldc
+git checkout 74630ed
+git submodule update --init
+mkdir build && cd build
+cmake ..
+make -j2
+sudo make install
