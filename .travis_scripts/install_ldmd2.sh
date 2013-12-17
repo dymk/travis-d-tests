960402f0d05785349477f2efdcc4f36b4afce3f5
Dylan Knutson <tcdknutson@gmail.com>
2013-12-16 18:55:47 -0800

    ++, actually

--- .travis_scripts/install_ldmd2.sh
+++ .travis_scripts/install_ldmd2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install cmake llvm-3.2 libconfig-dev
+sudo apt-get install cmake llvm-3.2 libconfig++-dev
 git clone https://github.com/ldc-developers/ldc.git ldc
 cd ldc
 git checkout 74630ed
