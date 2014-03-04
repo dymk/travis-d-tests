5589ef1cce3e9338e45b6eee8d0bdbb2b2b3e18f
Dylan Knutson <tcdknutson@gmail.com>
2014-03-04 14:25:26 -0800

    Sudo the ln

--- .travis_scripts/install_ldmd2.sh
+++ .travis_scripts/install_ldmd2.sh
@@ -2,4 +2,4 @@
 
 wget --no-check-certificate https://github.com/ldc-developers/ldc/releases/download/v0.13.0-alpha1/ldc2-0.13.0-alpha1-linux-x86_64.tar.gz
 tar -xzf ldc2-0.13.0-alpha1-linux-x86_64.tar.gz
-ln -s `pwd`/ldc2-0.13.0-linux-x86_64/bin/ldmd2 /usr/bin/ldmd2
+sudo ln -s `pwd`/ldc2-0.13.0-linux-x86_64/bin/ldmd2 /usr/bin/ldmd2
